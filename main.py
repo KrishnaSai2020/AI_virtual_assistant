9b4070a4700729ca7ea69f7acfd6799b30c7d11e
KrishnaSai2020 <krishnasai.chivukula@gmal.com>
2021-07-23 21:19:29 +0100

    simple dummy code from the internet inserted as a very rough idea. Will rewrite the whole lot from scratch this is just a template.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import speechRecognition as sr
+import SpeechRecognition as sr
 import pyttsx3
 import datetime
 import wikipedia
@@ -11,4 +11,59 @@ import wolframalpha
 import json
 import requests
 
-print('hello!')
+# voice engine setup
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+engine.setProperty('voice', 'voices[0].id')
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+
+def Greeting():
+    hour = datetime.datetime.now().hour
+    if 0 <= hour < 12:
+        speak("Hello,Good Morning")
+        print("Hello,Good Morning")
+    elif 12 <= hour < 18:
+        speak("Hello,Good Afternoon")
+        print("Hello,Good Afternoon")
+    else:
+        speak("Hello,Good Evening")
+        print("Hello,Good Evening")
+
+
+def Get_User_Command():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        audio = r.listen(source)
+
+        try:
+            statement = r.recognize_google(audio, language='en-in')
+            print(f"user said:{statement}\n")
+
+        except Exception as e:
+            speak("Pardon me, please say that again")
+            return "None"
+        return statement
+
+
+print("Loading your AI personal assistant G-One")
+speak("Loading your AI personal assistant G-One")
+Greeting()
+
+if __name__=='__main__':
+
+    while True:
+        speak("Tell me how can I help you now?")
+        statement = Get_User_Command().lower()
+        if statement==0:
+            continue
+
+        if "good bye" in statement or "ok bye" in statement or "stop" in statement:
+            speak('your personal assistant G-one is shutting down,Good bye')
+            print('your personal assistant G-one is shutting down,Good bye')
+            break
