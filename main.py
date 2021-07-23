fc7a16de9fa1ff27791b2113aa88d7eff71dc5b9
KrishnaSai2020 <krishnasai.chivukula@gmal.com>
2021-07-23 21:05:12 +0100

    intialisation

---
+++ main.py
@@ -0,0 +1,14 @@
+import speechRecognition as sr
+import pyttsx3
+import datetime
+import wikipedia
+import webbrowser
+import os
+import time
+import subprocess
+from ecapture import ecapture as ec
+import wolframalpha
+import json
+import requests
+
+print('hello!')
